68074d6b2b04322ce7147f4259b0c3e94551a2f3
2º Téc. Desenvolvimento de Sistemas <ds-tec-2@servidor.acad>
2023-06-27 10:37:42 -0300

    v3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $("#btnDireita").click(
     function () {
-        $("#virus").animate({ left: "900px" }), ({ duration: 1000 })
+        $("#virus").animate({ left: "1300px" }), ({ duration: 6000 })
     }
 )
 
 $("#btnEsquerda").click(
     function () {
-        $("#virus").animate({ left: "0px" }), ({ duration: 1000 })
+        $("#virus").animate({ left: "0px" }), ({ duration: 3000 })
     }
 )
